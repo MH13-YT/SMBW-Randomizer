e67c28218c996f0166db98cd2dff6c1afae8733c
MH13 <mathias.tavernier1@gmail.com>
2023-10-30 16:21:24 +0100

    Hotfix errors
    Random seed causes errors
    
    Installation via winget cause issues (disabling it)

--- install.bat
+++ install.bat
@@ -3,40 +3,44 @@ cls
 echo SMBW Randomizer, dependencies installer
 echo -----------------------------------------
 echo The following packages will be installed on your computer and are necessary to run the SMBW Randomizer:
-echo - Python: Program: Source Winget "https://apps.microsoft.com/search/publisher?name=Python+Software+Foundation&hl=en-US&gl=US"
 echo - PyYAML: Python Module: Source PIP "https://pypi.org/project/PyYAML/"
 echo - byml: Python Module with executables: Source PIP "https://pypi.org/project/byml/"
 echo.
+echo Before start script please 
 pause
 
-REM Installing Python with Winget
-winget install Python
-
-REM Installing Python dependencies from requirements.txt
-pip install -r requirements.txt
-
-echo Installation completed. Please check if Python executables are startable.
-
-REM Checking the byml_to_yml executable
-where byml_to_yml > nul
+where pip > nul
 if %errorlevel%==0 (
-    echo byml_to_yml: Test Passed
-) else (
-    echo byml_to_yml: Test Failed
-    echo Installation failed, please retry with manual installation.
-    pause
-    exit /b 1
+    REM Installing Python dependencies from requirements.txt
+    pip install -r requirements.txt
+
+    echo Installation completed. Please check if Python executables are startable.
+
+    REM Checking the byml_to_yml executable
+    where byml_to_yml > nul
+    if %errorlevel%==0 (
+        echo byml_to_yml: Test Passed
+    ) else (
+        echo byml_to_yml: Test Failed
+        echo Installation failed, please retry with manual installation.
+        pause
+        exit /b 1
+    )
+
+    REM Checking the yml_to_byml executable
+    where yml_to_byml > nul
+    if %errorlevel%==0 (
+        echo yml_to_byml: Test Passed
+    ) else (
+        echo yml_to_byml: Test Failed
+        echo Installation failed, please retry with manual installation.
+        pause
+        exit /b 1
+    )
 )
-
-REM Checking the yml_to_byml executable
-where yml_to_byml > nul
-if %errorlevel%==0 (
-    echo yml_to_byml: Test Passed
-) else (
-    echo yml_to_byml: Test Failed
-    echo Installation failed, please retry with manual installation.
-    pause
-    exit /b 1
+else
+(
+    echo Python is not installed, or installed without PATH please install from Python Website or modify your installation
 )
 
 pause
