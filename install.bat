cd0b9622510b5a580bff75e311004b26cde06d2e
MH13 <mathias.tavernier1@gmail.com>
2023-10-30 08:07:50 +0100

    Fix error on install and run script

--- install.bat
+++ install.bat
@@ -1,32 +1,42 @@
-echo off
+@echo off
 cls
-echo SMBW Randomizer, dependances installer
-echo ---------------------------------------
-echo Theses packages will be installed on your computer, and are necessary for run SMBW_Randomizer
-echo - Python : Program : Source Winget "https://apps.microsoft.com/search/publisher?name=Python+Software+Foundation&hl=fr-fr&gl=FR"
-echo - PyYAML : Python Module : Source PIP "https://pypi.org/project/PyYAML/"
-echo - byml : Python Module with executables : Source PIP "https://pypi.org/project/byml/"
-echo
+echo SMBW Randomizer, dependencies installer
+echo -----------------------------------------
+echo The following packages will be installed on your computer and are necessary to run the SMBW Randomizer:
+echo - Python: Program: Source Winget "https://apps.microsoft.com/search/publisher?name=Python+Software+Foundation&hl=en-US&gl=US"
+echo - PyYAML: Python Module: Source PIP "https://pypi.org/project/PyYAML/"
+echo - byml: Python Module with executables: Source PIP "https://pypi.org/project/byml/"
+echo.
 pause
-winget install python
+
+REM Installing Python with Winget
+winget install Python
+
+REM Installing Python dependencies from requirements.txt
 pip install -r requirements.txt
-echo Installation Finished, verify if Python Executables is startable
+
+echo Installation completed. Please check if Python executables are startable.
+
+REM Checking the byml_to_yml executable
 where byml_to_yml > nul
 if %errorlevel%==0 (
-    echo byml_to_yml : Test Passed
+    echo byml_to_yml: Test Passed
 ) else (
-    echo byml_to_yml : Test Failed
-    echo Installation Failed, please retry with manual installation
+    echo byml_to_yml: Test Failed
+    echo Installation failed, please retry with manual installation.
     pause
-    exit
+    exit /b 1
 )
+
+REM Checking the yml_to_byml executable
 where yml_to_byml > nul
 if %errorlevel%==0 (
-    echo yml_to_byml : Test Passed
+    echo yml_to_byml: Test Passed
 ) else (
-    echo yml_to_byml : Test Failed
-    echo Installation Failed, please retry with manual installation
+    echo yml_to_byml: Test Failed
+    echo Installation failed, please retry with manual installation.
     pause
-    exit
+    exit /b 1
 )
+
 pause
--- run.bat
+++ run.bat
@@ -1,16 +1,16 @@
-echo off
+@echo off
 cls
 where byml_to_yml > nul
-if %errorlevel%!==0 (
+if %errorlevel% neq 0 (
     echo byml_to_yml : Test Failed
-    echo Cannot start randomizer without this executable, try to run install.bat
+    echo Cannot start the randomizer without this executable, try running install.bat
     pause
     exit
 )
 where yml_to_byml > nul
-if %errorlevel%!==0 (
+if %errorlevel% neq 0 (
     echo yml_to_byml : Test Failed
-    echo Cannot start randomizer without this executable, try to run install.bat
+    echo Cannot start the randomizer without this executable, try running install.bat
     pause
     exit
 )
